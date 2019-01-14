d2e404dfe67df2cd0eb3f1dfbdc82695c17f6518
Gilang Nuril Haqim <gilangn89@gmail.com>
2019-01-14 13:17:48 +0800

    Updates

---
+++ precache-manifest.6c145249d2957503b89e93cacc61246d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9668e772d8ce539e23c3",
+    "url": "/chattt/static/js/runtime~main.9668e772.js"
+  },
+  {
+    "revision": "31087f20164d81525e5f",
+    "url": "/chattt/static/js/main.31087f20.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/chattt/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "31087f20164d81525e5f",
+    "url": "/chattt/static/css/main.a513dc5a.chunk.css"
+  },
+  {
+    "revision": "061465c21ef3fbfbd873e893fb14aba0",
+    "url": "/chattt/index.html"
+  }
+];
